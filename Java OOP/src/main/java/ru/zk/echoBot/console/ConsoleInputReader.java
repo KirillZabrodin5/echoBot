3485a52430f303845a0792171975bd1896dab72b
KirillZabrodin5 <fea.z@yandex.ru>
2023-11-02 12:07:53 +0500

    окончательная версия эхо бота

--- Java OOP/src/main/java/ru/zk/echoBot/console/ConsoleInputReader.java
+++ Java OOP/src/main/java/ru/zk/echoBot/console/ConsoleInputReader.java
@@ -6,12 +6,7 @@ import ru.zk.echoBot.logic.BotRequest;
 import java.util.Scanner;
 
 public class ConsoleInputReader implements InputReader {
-    private Scanner scanner;
-
-    public ConsoleInputReader() {
-        this.scanner = new Scanner(System.in);
-    }
-
+    private final Scanner scanner = new Scanner(System.in);;
     public BotRequest getUserInput() {
         System.out.print("Enter your message: ");
         return new BotRequest(scanner.nextLine());
--- Java OOP/src/main/java/ru/zk/echoBot/logic/BotRequest.java
+++ Java OOP/src/main/java/ru/zk/echoBot/logic/BotRequest.java
@@ -2,11 +2,9 @@ package ru.zk.echoBot.logic;
 
 public class BotRequest {
     private String message;
-
     public BotRequest(String message) {
         this.message = message;
     }
-
     public String getMessage() {
         return message;
     }
